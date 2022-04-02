d32797e5e62ce22df810aaeff6817e2bbcc84f8e
Luis Henrique <luish.fa1515@gmail.com>
2022-04-02 19:53:12 -0300

    Setting routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,23 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 
 const routes = Router();
+
+routes.get("/", (req: Request, res: Response) => {
+	return res.send("Home page");
+});
+
+routes.get("/create", (req: Request, res: Response) => {
+	return res.send("Shorten URL");
+});
+
+routes.post("/a/create", (req: Request, res: Response) => {
+	return res.send("Shorten URL POST");
+});
+
+routes.get("/p/:page", (req: Request, res: Response) => {
+	const page = req.params.page
+	
+	return res.send("Page: " + page);
+});
 
 export { routes };
