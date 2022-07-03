e716c8173f50ea958cc226fd26ac4eb3edb7b46a
Luis Henrique <luish.fa1515@gmail.com>
2022-07-03 20:00:07 -0300

    Changing repo main idea

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,6 @@
 import { Router, Request, Response } from "express";
 
-import { RenderPageController } from "./controllers/RenderPageController";
-
-const renderPageController = new RenderPageController();
-
 const routes = Router();
-
-routes.get("/", renderPageController.handle("home.html"));
-routes.get("/create", renderPageController.handle("create.html"));
 
 routes.post("/a/create", (req: Request, res: Response) => {
 	return res.send("Shorten URL POST");
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,9 @@
-import path from "path";
 import express from "express";
 import { routes } from "./routes";
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "..", 'public')));
-app.set('views', path.join(__dirname, "..", 'public'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
+app.use(express.json());
 app.use(routes);
 
 app.listen(3000, () => {
